3c958d72d7477fd2d2082f9c1b9ec5a6a89e436e
Saimon Tolazzi <saimontolaz@hotmail.com>
2022-08-17 17:03:46 -0300

    atividade_01

---
+++ bimestre_01-main/bimestre_01-main/atividade_01/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./pirulito.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Saimon Tolazzi</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
